e381061b9bc8a5775759c6cb340f512de36d4e73
marin <marin@test.com>
2024-02-16 14:54:56 +0200

    First test added

--- src/test/java/testCases/GetStarsTitleHomepage.java
+++ src/test/java/testCases/GetStarsTitleHomepage.java
@@ -10,7 +10,7 @@ public class GetStarsTitleHomepage extends BaseTest {
     @Test
     public void getStarCategory() {
         boolean isCategoryListed = driver.findElement(By.xpath(ORproperties.getProperty("starSigns"))).isDisplayed();
-        Assert.assertTrue(isCategoryListed, "Category is missing on the homepage");
+        Assert.assertTrue(isCategoryListed, "Category is missing on the homepage"); // Test OK
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
