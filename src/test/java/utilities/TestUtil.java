3229dfd387293a0034e9ec2e6fb412ec7bf2c693
marin <marin@test.com>
2024-02-21 08:39:36 +0200

    Added changes: 1. Listeners

---
+++ src/test/java/utilities/TestUtil.java
@@ -0,0 +1,34 @@
+package utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.Hashtable;
+
+import base.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.DataProvider;
+
+public class TestUtil extends BaseTest {
+
+    public static String screenshotPath;
+    public static String screenshotName;
+
+    public static void captureScreenshot() throws IOException {
+
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        Date d = new Date();
+        screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+        FileUtils.copyFile(scrFile,
+                new File(System.getProperty("/target/surefire-reports/html/") + screenshotName));
+        FileUtils.copyFile(scrFile,
+                new File("./reports/" + screenshotName));
+
+    }
+
+}
